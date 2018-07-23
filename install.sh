98670030b2240ade5235b089b1fc44fd74af6880
Dru Jensen <drujensen@gmail.com>
2018-07-23 11:03:22 -0700

    fix install script

--- install.sh
+++ install.sh
@@ -1,3 +1,29 @@
-curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh
-apt-vim install -y git://github.com/rking/ag.vim.git git://github.com/bling/vim-airline.git git://github.com/Chiel92/vim-autoformat.git git://github.com/tpope/vim-bundler.git git://github.com/rhysd/vim-crystal.git git://github.com/kien/ctrlp.vim.git git://github.com/tpope/vim-cucumber.git git://github.com/tpope/vim-fugitive.git git://github.com/airblade/vim-gitgutter.git git://github.com/gregsexton/gitv.git git://github.com/pangloss/vim-javascript.git git://github.com/mxw/vim-jsx.git git://github.com/JamshedVesuna/vim-markdown-preview.git git://github.com/terryma/vim-multiple-cursors.git git://github.com/ddollar/nerdcommenter.git git://github.com/scrooloose/nerdtree.git git://github.com/jistr/vim-nerdtree-tabs.git git://github.com/tpope/vim-rails.git git://github.com/vim-ruby/vim-ruby.git git://github.com/drujensen/screen.git git://github.com/isaacsloan/vim-slang.git git://github.com/scrooloose/syntastic.git git://github.com/majutsushi/tagbar.git git://github.com/drujensen/vim-test-recall.git git://github.com/jgdavey/tslime.vim.git git://github.com/tc50cal/vim-terminal.git
+# curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh
 
+apt-vim install -y \
+  git://github.com/rking/ag.vim.git \
+  git://github.com/bling/vim-airline.git \
+  git://github.com/Chiel92/vim-autoformat.git \
+  git://github.com/tpope/vim-bundler.git \
+  git://github.com/rhysd/vim-crystal.git \
+  git://github.com/kien/ctrlp.vim.git \
+  git://github.com/tpope/vim-cucumber.git \
+  git://github.com/tpope/vim-fugitive.git \
+  git://github.com/airblade/vim-gitgutter.git \
+  git://github.com/gregsexton/gitv.git \
+  git://github.com/pangloss/vim-javascript.git \
+  git://github.com/mxw/vim-jsx.git \
+  git://github.com/JamshedVesuna/vim-markdown-preview.git \
+  git://github.com/terryma/vim-multiple-cursors.git \
+  git://github.com/ddollar/nerdcommenter.git \
+  git://github.com/scrooloose/nerdtree.git \
+  git://github.com/jistr/vim-nerdtree-tabs.git \
+  git://github.com/tpope/vim-rails.git \
+  git://github.com/vim-ruby/vim-ruby.git \
+  git://github.com/drujensen/screen.git \
+  git://github.com/isaacsloan/vim-slang.git \
+  git://github.com/scrooloose/syntastic.git \
+  git://github.com/majutsushi/tagbar.git \
+  git://github.com/drujensen/vim-test-recall.git \
+  git://github.com/jgdavey/tslime.vim.git \
+  git://github.com/tc50cal/vim-terminal.git
