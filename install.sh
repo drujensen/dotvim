caad6370fb841b64597c2ea914934fe5379bb0d2
Dru Jensen <drujensen@gmail.com>
2021-11-14 02:30:16 +0000

    fix: remove Coc

--- install.sh
+++ install.sh
@@ -31,29 +31,3 @@ apt-vim install -y \
   git://github.com/tpope/vim-fireplace.git \
   git@github.com:kien/rainbow_parentheses.vim.git
 
-# Use package feature to install coc.nvim
-# for vim8
-mkdir -p ~/.vim/pack/coc/start
-cd ~/.vim/pack/coc/start
-curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
-
-# Install extensions
-mkdir -p ~/.config/coc/extensions
-cd ~/.config/coc/extensions
-if [ ! -f package.json ]
-then
-  echo '{"dependencies":{}}'> package.json
-fi
-# Change extension names to the extensions you need
-# https://github.com/neoclide
-npm install coc-marketplace
-npm install coc-solargraph #ruby gem install solargraph
-npm install coc-java
-npm install coc-python
-npm install coc-eslint
-npm install coc-css
-npm install coc-yaml
-npm install coc-json
-npm install coc-jest
-npm install coc-go
-npm install coc-conjure
