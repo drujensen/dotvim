9d60b61ab8acfe5f45ccfddcb079d32bda2c2945
Dru Jensen <drujensen@gmail.com>
2024-12-01 14:43:43 +0000

    fix: languages

--- init.lua
+++ init.lua
@@ -330,7 +330,7 @@ if is_plugins_installed() then
   require('mason-lspconfig').setup({
     ensure_installed = {
       'rust_analyzer', 'gopls', 'jdtls', 'gradle_ls','clojure_lsp',
-      'eslint', 'tsserver', 'pylsp', 'ruby_ls', 'rubocop',
+      'eslint', 'ts_ls', 'pylsp', 'ruby_lsp', 'rubocop',
       'yamlls', 'jsonls', 'taplo', 'cssls', 'html', 'sqlls',
       'dockerls', 'terraformls', 'vimls', 'bashls', 'omnisharp',
     },
